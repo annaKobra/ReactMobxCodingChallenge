3af19b3d546458a2379c673d820eb20939f3fce4
annaKobra <annakobrinsky@gmail.com>
2021-06-04 10:23:01 +0300

    Create function to update userModel

--- updateCurrentUser.js
+++ updateCurrentUser.js
@@ -1,5 +1,21 @@
-// updateCurrentUser = (currentUserID) => {
-//     try {
-//   };
+import User from './userModel';
+
+updateCurrentUser = async (currentUserID, name="", age=0) => {
+    try {
+        const user = await User.findOneAndUpdate(currentUserID, {name, age});
+
+        if(!user) {
+            const newUser = new User({
+                _id: currentUserID,
+                name: name,
+                age: age
+            })
+            return await newUser.save();
+        }
+        return user;
+    } catch(err) {
+        return {error: err};
+    }
+}
 
 // Please comment out all your code when you are finished.
--- userModel.js
+++ userModel.js
@@ -1,19 +1,17 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-// const UserSchema = new Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     age: {
-//         type: Number,
-//         required: true
-//     }
-// }, { timestamps: true });
-
-
-// module.exports = mongoose.model('User', UserSchema);
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
 
 
+module.exports = mongoose.model('User', UserSchema);
 // Please comment out all your code when you are finished.
